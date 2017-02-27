05eefc817dd3117057a7e40d2f62dec742c2b2b1
sigmafsbot <sigmacabre@gmail.com>
2017-02-27 22:50:49 +0300

    Halfway through 'visibility' and 'call context' chapter

--- script1.js
+++ script1.js
@@ -1,5 +1,17 @@
-var planetName = "Земля";
-var userName = "Петя";
+var start = new Date; // засекли время
 
-alert(planetName)
-alert(userName)
+// что-то сделать
+result = 0;
+function sum(n) {
+	while (n > 0) {
+	  n -= 1;
+		result += function(n-1);
+    }
+	return result;
+}
+
+console.log("sum is: " + sum(10));
+
+var end = new Date; // конец измерения
+
+console.log( "Цикл занял " + (end - start) + " ms" );
